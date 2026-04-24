6fcb4579c9a7fb125a17b242afd1aa0c264ab571
jsniel <70667678+jsniel@users.noreply.github.com>
2026-04-24 22:53:24 +0200

    Update 3615-newsletter.ino

--- 3615-newsletter.ino
+++ 3615-newsletter.ino
@@ -1,21 +1,23 @@
-////////////////////////////////////////////////////////////////////////
 /*
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program. If not, see <http://www.gnu.org/licenses/>.
-   
-////////////////////////////////////////////////////////////////////////
-  DEBUT DU PROGRAMME 
-*///////////////////////////////////////////////////////////////////////
+ * Minitel Newsletter Arduino
+ * --------------------------
+ * 
+ * Saisie d'adresses email via Minitel 1B
+ * et enregistrement sur carte SD (CSV).
+ * 
+ * Matériel :
+ * - Arduino Nano / Uno
+ * - Minitel 1B
+ * - Module SD (SPI)
+ * - Interface Minitel (shield 3615 recommandé)
+ * 
+ * Librairie :
+ * Minitel1B_Soft
+ * https://github.com/eserandour/Minitel1B_Soft
+ * 
+ * Licence : GPLv3
+ * https://www.gnu.org/licenses/
+ */
 
 #include <Minitel1B_Soft.h>
 #include <SoftwareSerial.h>
